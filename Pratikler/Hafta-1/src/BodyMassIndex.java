6cfd260744b938713bc691ba60da02f80f47ff59
Eyüp BALCAN <balcan1905@gmail.com>
2023-05-16 16:58:59 +0300

    BodyMassIndex

---
+++ Pratikler/Hafta-1/src/BodyMassIndex.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        //Burada kullanıcadan kilo ve boyu alıyoruz
+        double kg,size,index;
+        Scanner input = new Scanner(System.in);
+        //Burada kullanıcadan kilo ve boyu alıyoruz
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        size = input.nextDouble();
+        //Burada yanlış boyu yanlış girmemesini sağlıyorum
+        if(size<2.5 && size>1)
+            {
+            System.out.print("Lütfen kilonuzu giriniz : ");
+
+            kg = input.nextDouble();
+            index= kg/(size*size);
+            System.out.println(index);
+            }
+
+        else {
+            System.out.println("Boyu yanlış girdiniz");
+        }
+
+
+
+    }
+}
