121554b35f8d26e3cdb65e890b54035f9087faa9
Eyüp BALCAN <balcan1905@gmail.com>
2023-05-19 16:04:05 +0300

    ForcesofNumbers

---
+++ Pratikler/Hafta-1/src/ForcesofNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ForcesofNumbers {
+    public static void main(String[] args) {
+        int n,total;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Sayı giriniz:");
+        n=input.nextInt();
+        System.out.println("4 sayısının kuvvetleri"100);
+        for(int i=1; i<=n;i*=4){
+            System.out.println(+i);
+
+        }
+        System.out.println("5 sayısının kuvvetleri");
+        for(int i=1; i<=n;i*=5){
+            System.out.println(+i);
+        }
+    }
+}
